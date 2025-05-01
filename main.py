72710e3909c68ca911da76aff9214e5320ec9ee8
Mikhail Komarov <144356904+hardWorker254@users.noreply.github.com>
2025-05-02 00:25:28 +0400

    Improvment structure

---
+++ main.py
@@ -0,0 +1,83 @@
+from g4f.client import Client
+from rich.console import Console
+from promt import Promt
+
+
+#  Collection of free and stable(no) models
+TEXT_MODELS = ["gpt-4o", "gpt-4o-mini", "llama-3.2-11b", 
+          "gemini-1.5-flash", "gemini-1.5-pro", "command-r(dumm)", 
+          "command-r-plus", "command-r7b", "command-a", 
+          "qwen-2-72b", "qwen-2.5-coder-32b", "qwen-2.5-1m", 
+          "deepseek-v3(sometimes)", "deepseek-r1(sometimes)",
+          "evil(18+ mode)"]
+
+
+# Here we go!
+data = input("You: ")
+
+# Formatting AI output, uploading files, truncates history and more
+promt = Promt()
+
+# For beautiful output
+console = Console()
+
+# GPT
+client = Client()
+
+while data != "exit":
+    
+    # User wants to change model
+    if data.split()[0] == "set_model":
+        promt.model = data.split()[1]
+        console.print(promt.format("Config:</think> ***setted model to " + data.split()[1] + "***"))
+        data = input("You: ")
+        continue
+    
+    # User wants to see all models
+    elif data == "model_list":
+        console.print(promt.format("Config:</think> ***all models list:***"))
+        for el in TEXT_MODELS:
+            console.print(promt.format("***" + el + "***"), end="\n")
+        data = input("You: ")
+        continue
+    
+    # User wants to clean history
+    elif data == "clean":
+        console.print(promt.format("Config:</think> ***history cleaned***"))
+        promt.history = []
+        data = input("You: ")
+        continue
+    
+    # Adding user promt
+    promt.content = data
+    
+    # Setting language
+    promt.language = "ru"
+    
+    # Formatting promt
+    promt.create()
+    
+    # No comments
+    console.print(promt.format(f"{promt.model}: ***typing...***"))
+    
+    # Getting AI's response
+    response = client.chat.completions.create(
+        model=promt.model,
+        messages=promt.history,
+        web_search=False
+    )
+    
+    # Adding to history
+    promt.history.append({"role": "assistant", "content": response.choices[0].message.content})
+    
+    # Deleting "typing..."
+    print("\033[F\033[K", end='')
+    
+    # Formatting output
+    formatted = promt.format(promt.history[-1]["content"])
+    
+    # We did it!
+    console.print(f"{promt.model}:", formatted)
+    
+    # Oh shit...
+    data = input("You: ")
---
+++ promt.py
@@ -0,0 +1,98 @@
+import os
+from rich.text import Text
+import re
+
+
+class Promt:
+    def __init__(self):
+        self.content = ""
+        self.model = "gpt-4o"
+        self.language = "ru"
+        self.language_ru = "Type the answer using the RUSSIAN LANGUAGE."
+        self.language_en = "Type the answer using the ENGLISH LANGUAGE."
+        self.history = []
+    
+    
+    def parse_files(self):
+        files = []
+        for el in self.content.split():
+            if os.path.isfile(el):
+                files.append(el)
+            elif os.path.isfile(el[:-1]):
+                files.append(el)
+        files_content = ""
+        for el in files:   
+            try:
+                open(el).close()
+                print("Attach file " + el + "?")
+                answer = input("[Y/N]: ")
+                if answer.lower() == "n":
+                    continue
+                files_content += el
+                files_content += "\n"
+                files_content += open(el).read()
+                files_content += "\n"
+            except:
+                print("Attach file " + el[:-1] + "?")
+                answer = input("[Y/N]: ")
+                if answer.lower() == "n":
+                    continue
+                files_content += el[:-1]
+                files_content += "\n"
+                files_content += open(el[:-1]).read()
+                files_content += "\n"
+            files_content += "\nEnd of " + el + "\n"
+        return files_content
+    
+    
+    def truncate_history(self):
+        if len(self.history) >= 6:
+            self.history.pop(0)
+            self.history.pop(0)
+    
+    
+    def create(self):
+        self.content += "\n"
+        if self.language == "ru":
+            self.content += self.language_ru
+        else:
+            self.content += self.language_en
+        self.content += "\n"
+        self.content += self.parse_files()
+        self.truncate_history()
+        self.history.append({"role": "user", "content": self.content.rstrip()})
+        return True
+    
+    
+    def format(self, text: str) -> Text:
+        if len(text) > 10 and text[:7:].lstrip().rstrip() == "<think>":
+            text = text[7::]
+        console_text = Text()
+        parts = re.split(r'(</think>)', text, maxsplit=1, flags=re.IGNORECASE)
+        if len(parts) > 1:
+            before_think = parts[0]
+            after_think = parts[2] if len(parts) > 2 else ""
+            before_text = self.parse_bold(before_think)
+            before_text.stylize("italic")
+            console_text.append(before_text)
+            after_text = self.parse_bold(after_think)
+            console_text.append(after_text)
+        else:
+            console_text = self.parse_bold(text)
+        return console_text
+
+
+    def parse_bold(self, text: str) -> Text:
+        result = Text()
+        pattern = re.compile(r'(\*\*\*|\*\*|`)(.+?)(\1)')
+        last_index = 0
+        for match in pattern.finditer(text):
+            start, end = match.span()
+            if start > last_index:
+                result.append(text[last_index:start])
+            bold_text = match.group(2)
+            result.append(bold_text, style="bold")
+            last_index = end
+        if last_index < len(text):
+            result.append(text[last_index:])
+        return result
